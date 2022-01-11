47e0f46ee9bf18f12a4797941f1ffe9e6277b5dd
Daniel Choi <xrontux@gmail.com>
2022-01-11 17:20:30 +0800

    website changes

---
+++ PersonalWebsite/index.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html>
+    <head><title>Daniel Choi</title></head>
+
+    <body>
+        <h3 style="text-align:center;"> DANIEL CHOI  </h3>
+
+        <p> <img src="/is216/myprofile-xrontux/PersonalWebsite/PizzaPhoto.jpg" 
+       width="250"  
+      height="300"/>
+         <br/>
+
+        Hello! My name is Daniel and i am a year 2 student from SCIS. I am an international student from South Korea and hope that 
+        i will be able to make friends with everyone in this classroom! 
+        <br/>
+        </p>
+        
+       <h1> My hobbies are : </h1> 
+            <br/>
+            <ul>
+            <li>Cooking</li>
+            <li>Tennis</li>
+            <li>Gymming</li>
+            </ul>
+
+        </p>
+
+    </body>
+
+
+
+
+
+
+</html>
+
